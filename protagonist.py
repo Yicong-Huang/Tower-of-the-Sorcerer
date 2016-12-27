5158c8661c38463c1997a5dccb4e7c49d976d696
Yicong-Huang <yiconghuang@Yicongs-MacBook-Pro.local>
2016-12-27 23:58:28 +0800

    main character

--- protagonist.py
+++ protagonist.py
@@ -14,29 +14,29 @@ from mob import Mob
 
 class Protagonist(Character):
     def __init__(self,name,tower,**kargs):
-        
+
         values=ele_lib.lib[name]
         self._tower=tower
         for key,value in kargs.items():
             exec('self._'+key+'='+value)
-        Character.__init__(self,self._i,self._j,name,tower.current_floor())
+        Character.__init__(self,self._i,self._j,name,tower.current)
 
 
-        
+
     def change_image(self,direction):
-        
+
         self._image=image
-        
-        
+
+
     def move(self,di,dj):
 
         temp_i=self._i+di
         temp_j=self._j+dj
-        
-        self._floor = self._tower.current_floor()
+
+        self._floor = self._tower.current
         attempt=self._floor.get_floor(temp_i,temp_j)
 
-        
+
         if attempt and not isinstance(attempt,str):
             print(attempt._name)
         self._moved=False
@@ -49,11 +49,11 @@ class Protagonist(Character):
             #print(attempt.__dict__)
             attempt=eval(type(ele_lib.lib[attempt._obj]).__name__+'(temp_i,temp_j,attempt._obj,self._floor)')
 
-           
-       
+
+
         if attempt and not isinstance(attempt,str):
             attempt.interact(self)
-                    
+
         else:
             self._moved=True
         if not self._magic_defense and self._floor._num>40:
@@ -63,7 +63,7 @@ class Protagonist(Character):
                     if 'pri_witch' in test._name:
                         self._hp-=100
                     elif 'pro_witch' in test._name:
-                        self._hp-=200    
+                        self._hp-=200
                     elif 'magic_guard' in test._name:
                         oppo_test=self._floor.get_floor(temp_i-i,temp_j-j)
                         if oppo_test and not isinstance(oppo_test, str) and 'magic_guard' in oppo_test._name:
@@ -71,10 +71,10 @@ class Protagonist(Character):
                             break
         if self._moved and 0<=temp_i<11 and 0<=temp_j<11:
             self._i,self._j=temp_i,temp_j
-            
+
         if self._interacted:
             self._floor.del_floor(self._i,self._j)
-        
+
         if self._case:
             if self._floor._num==3:
                 self._floor=self._tower.previous_floor()
@@ -84,9 +84,9 @@ class Protagonist(Character):
                 self._defense=10
             else:
                 Plot(temp_i,temp_j,self._floor,self._case,self)
-        
 
-            
+
+
     def __str__(self):
         if not self._hallows:
             hallows='set()'
@@ -98,8 +98,3 @@ class Protagonist(Character):
             self._red_key,self._shop,self._cross,self._lucky_coin,
             self._dragon_dagger,self._magic_defense,hallows
             ]])
-        
-        
-
-        
-        
