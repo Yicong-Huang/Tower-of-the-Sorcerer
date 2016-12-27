276acd6e765a80c7e6a320724c40ba4b3cc463ff
Yicong-Huang <yiconghuang@Yicongs-MacBook-Pro.local>
2016-12-27 18:11:23 +0800

    base class Obj

--- obj.py
+++ obj.py
@@ -1,19 +1,14 @@
+'''
+Defining the base class Obj for all classes
+'''
 class Obj:
-    
+    '''everything of the game is a Obj object.'''
     side = 32
-    
-    def __init__(self,i,j):
-        
-        self._i,self._j = i,j
-    
-    
-    
+    def __init__(self, i, j):
+        self._i, self._j = i, j
     def get_position(self):
-        return self._i,self._j
-    
-    def set_position(self,i,j):
-        self._i,self.j=i,j
-#     def get_name(self):
-#         return self._name
-    
-    
+        "returns the i,j position of the Obj"
+        return self._i, self._j
+    def set_position(self, i, j):
+        "sets the i,j position of the Obj as provided"
+        self._i, self._j = i, j
