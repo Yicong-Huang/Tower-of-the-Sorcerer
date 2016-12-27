c10e1bd4f9655cca2c7773185436ea3cd25fbb61
Yicong-Huang <yiconghuang@Yicongs-MacBook-Pro.local>
2016-12-27 23:58:16 +0800

    outside model

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-import controller, view, ele_lib 
+import controller, view, ele_lib
 
 from floor import Floor
 from tower import Tower
@@ -21,40 +21,40 @@ def world():
 #     global F
 #     controller.the_canvas.delete('redraw')
 #     F=Floor(1)
-#     
-# 
+#
+#
 # #start running the simulation
 # def save ():
 #     F.save_floor()
 def save():
     T.save_tower(prot)
-    
-     
+
+
 def load():
     global T,F,prot,main
 
     for hallow in prot._hallows:
         controller.the_canvas.delete(hallow)
     T,prot=Tower.load_tower('game1.sav')
-    
+
     F=T.current_floor()
     display_all(controller.tick)
-    
+
     for hallow in prot._hallows:
         hallow=Hallow(0,0,hallow,F)
         ele_lib.set_image(hallow)
         hallow.interact(prot)
-    
-    
 
-    
+
+
+
 
 def show_info():
     return Hallow(0,0,'info_book',None).clicked(prot,None)
 
 def next ():
     T.next_floor()
-     
+
 def previous():
     T.previous_floor()
 
@@ -71,12 +71,12 @@ T,prot=Tower.load_tower('all_floors.flo')
 check=True
 def display_all(tick):
 
-    
+
     global main,check
     main=controller.the_canvas
-    floor = T.current_floor()
-    
-   
+    floor = T.current
+
+
     check=False
 
     main.delete('redraw')
@@ -89,18 +89,15 @@ def display_all(tick):
         item.display(main,tick)
 
     prot.display(main,tick)
-    
-    
-    
+
+
+
     main.create_text(79,87,text=prot.get_value('hp'),tag='redraw')
     main.create_text(79,111,text=prot.get_value('attack'),tag='redraw')
     main.create_text(79,135,text=prot.get_value('defense'),tag='redraw')
     main.create_text(79,159,text=prot.get_value('gold'),tag='redraw')
     main.create_text(65,57,text='Floor %s'% prot.get_value('floor._num'),tag='redraw')
-    
+
     main.create_text(560,155,text='       *    %s'% prot.get_value('yellow_key'),tag='redraw')
     main.create_text(560,175,text='       *    %s'% prot.get_value('blue_key'),tag='redraw')
     main.create_text(560,195,text='       *    %s'% prot.get_value('red_key'),tag='redraw')
-   
-   
-
